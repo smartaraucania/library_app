f889447e610f9893dfdcd581f4e6e4f481008e57
seba1696 <s.gonzalez11@ufromail.cl>
2019-11-13 15:48:40 -0300

    change sub-name application

--- lib/util/const.dart
+++ lib/util/const.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 class Constants{
 
-  static String appName = "Foody Bite";
+  static String appName = "Library App";
 
   //Colors for theme
   static Color lightPrimary = Color(0xfffcfcff);
